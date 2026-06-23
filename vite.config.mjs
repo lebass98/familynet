b220e31c19b7a5a7cebbe0532582ea013abf1b0e
이재광 <yellow@wordncode.com>
2026-06-23 16:49:57 +0900

    feat: update project assets, layouts, and build dependencies

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,11 +3,21 @@ import nunjucks from 'vite-plugin-nunjucks';
 import path from 'path';
 
 export default defineConfig({
+  base: './',
   plugins: [
     nunjucks({
       templatesDir: path.resolve(process.cwd(), 'templates')
     })
   ],
+  build: {
+    outDir: 'docs',
+    rollupOptions: {
+      input: {
+        main: path.resolve(process.cwd(), 'index.html'),
+        sub_page: path.resolve(process.cwd(), 'sub_page.html')
+      }
+    }
+  },
   server: {
     port: 3000,
     open: true
